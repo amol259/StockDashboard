80e12121179a6a9105a029c21b950445bcc12213
Sidhu <ass140230@utdallas.edu>
2016-09-01 20:45:09 -0500

    added web api and fixed routing

--- StockDashboard/StockDashboard/Controllers/LocationsController.cs
+++ StockDashboard/StockDashboard/Controllers/LocationsController.cs
@@ -5,18 +5,40 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+
 namespace StockDashboard.Controllers
 {
     public class LocationsController : ApiController
     {
+        [HttpGet()]
+        [ActionName("GetLocations")]// name for the action
+
+        public IEnumerable<Models.Locations> GetLocations()
+        {
+            List<Models.Locations> locations = new List<Models.Locations>();
+
+            locations = new List<Models.Locations>();
+            locations.Add(new Models.Locations()
+            {
+                LocationID = "1",
+                LocationName = "Warehouse 1"
+            });
+            locations.Add(new Models.Locations()
+            {
+                LocationID = "2",
+                LocationName = "US Warehouse 2"
+            });
+            locations.Add(new Models.Locations()
+            {
+                LocationID = "3",
+                LocationName = "Asia"
+            });
+            return locations;
 
 
 
 
 
-
-
-
-
+        }
     }
 }
